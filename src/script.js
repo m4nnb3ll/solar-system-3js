2aeafbb5b4f1c17ef7dac1acead26b0a0ac3fb8c
m4nnb3ll <abderrahmane.bell4@gmail.com>
2024-02-10 18:45:16 +0100

    improved the styling, about to include threejs

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 document.getElementById('toggle-expand').addEventListener('click', (e) => {
 	const mainCanvas = document.getElementById('canvas-container');
 	mainCanvas.style.height = mainCanvas.style.height != '100vh' ? '100vh' : '50vh';
+	e.target.classList.toggle("shrink");
 })
